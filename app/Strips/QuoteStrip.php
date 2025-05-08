5dd11996177a31f8cbb7fd4cd6721ef4421b0d89
Menno Tempelaar <menno@made-foryou.nl>
2025-05-08 16:14:58 +0200

    ✨ Add QuoteStrip component with view and configuration for displaying quotes

---
+++ app/Strips/QuoteStrip.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Strips;
+
+use Filament\Forms\Components\FileUpload;
+use Made\Cms\Filament\Builder\ContentStrip;
+
+class QuoteStrip implements ContentStrip
+{
+    public static function id(): string
+    {
+        return 'quote';
+    }
+
+    public static function render(array $attributes = []): \Illuminate\Contracts\View\View
+    {
+        $attributes['live'] = true;
+
+        return view('strips.' . self::id(), $attributes);
+    }
+
+    public static function block(string $context = 'form'): \Filament\Forms\Components\Builder\Block
+    {
+        return \Filament\Forms\Components\Builder\Block::make(self::id())
+            ->label('Quote')
+            ->icon('heroicon-s-chat-bubble-bottom-center-text')
+            ->schema([
+                \Filament\Forms\Components\Textarea::make('quote')
+                    ->label('Quote')
+                    ->required(),
+                \Filament\Forms\Components\TextInput::make('author')
+                    ->label('Auteur')
+                    ->required(),
+                
+                FileUpload::make('image')
+                    ->label('Afbeelding')
+                    ->image()
+                    ->imageEditor()
+                    ->imageEditorAspectRatios([
+                        '116:65',
+                        '547:175',
+                    ])
+                    ->preserveFilenames()
+                    ->required(),
+            ]);
+    }
+}
---
+++ resources/views/strips/quote.blade.php
@@ -0,0 +1,46 @@
+<section
+    class="relative
+           w-full
+           py-12
+           lg:py-18"
+>
+    <x-container class="max-w-6xl overflow-hidden rounded-lg">
+
+        @php($image = ($live) ? asset("storage/{$image}") : asset('storage/' . array_pop($image)))
+
+        <div 
+            class="relative w-full py-9 px-6
+                   md:py-29 md:px-23
+                   bg-cover bg-center bg-no-repeat
+                   aspect-116/65 lg:aspect-541/175
+                   rounded-lg"
+            style="background-image: url('{{ $image }}')"
+        >
+
+            <div class="relative z-10 mb-6">
+                <p 
+                    class="font text-white text-lg font-sans tracking-wide
+                           md:text-3xl"
+                >
+                    {!! nl2br($quote) !!}
+                </p>
+            </div>
+            <div class="relative z-10">
+                <p 
+                    class="font-light text-white text-sm font-sans tracking-wide 
+                           md:text-lg"
+                >
+                    {{ $author }}
+                </p>
+            </div>
+
+            <div 
+                class="absolute z-1 top-0 left-0
+                       w-full h-full rounded-lg
+                       bg-linear-to-bl from-transparent to-black/45"
+            ></div>
+
+        </div>
+
+    </x-container>
+</section>
