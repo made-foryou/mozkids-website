3a96cabaf00b027d50f2f39f0de437b3b974eb39
Menno Tempelaar <menno@made-foryou.nl>
2025-05-15 23:01:38 +0200

    ✨ Implements news overview page
    Adds a news overview page that displays a list of news posts.
    The news posts are fetched using the MadeNews facade.
    A new view component `OverviewItem` is introduced to render each news item.
    The page integrates with the existing page template and utilizes content strips for additional content.

---
+++ resources/views/pages/news/index.blade.php
@@ -0,0 +1,22 @@
+@extends('templates.page', [
+    'meta' => $model->meta,
+    'donation_button' => $donation_button,
+])
+
+@php
+    use Made\Cms\Facades\Cms;
+@endphp
+
+@section('content')
+
+    @include('strips.hero-strip', ['content' => 'Nieuwsberichten', 'buttons' => []])
+
+    @include('partials.news.overview', ['paddingTop' => false, 'posts' => $posts])
+
+    @if ($model && $model->content)
+
+        {!! Cms::renderContentStrips($model->content) !!}
+        
+    @endif
+
+@endsection
