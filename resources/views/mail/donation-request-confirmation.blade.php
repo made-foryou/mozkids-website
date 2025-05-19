103a659a06da72f2e7df2fd973b13b4e71159102
Menno Tempelaar <menno@made-foryou.nl>
2025-05-19 11:47:39 +0200

    ✨ Sends donation confirmation email to user
    Adds a confirmation email to be sent to the user upon successful submission of the donation request form.
    
    This allows the user to have a record of their submission and provides reassurance that their request has been received.

---
+++ resources/views/mail/donation-request-confirmation.blade.php
@@ -0,0 +1,36 @@
+<x-mail::message>
+# Bedankt voor je donatie
+
+Jouw donatie aanvraag is met succes naar ons verstuurd. Hieronder vind je een overzicht van jouw 
+ingevulde gegevens. Mocht je nog vragen en/of opmerkingen hebben dan kun je altijd even mailen 
+naar <a href="mailto:info@mozkids.nl">info@mozkids.nl</a>. 
+
+Voor de verwerking van jouw donatie zullen wij nog contact met je opnemen.
+
+### Jouw opmerkingen:
+
+{{ $data->comments ?? 'Geen opmerkingen ingevuld.' }}
+
+### Jouw gegevens:
+
+<x-mail::table>
+| Veld                 | Invulling                                         |
+| :------------------- | ------------------------------------------------: |
+| Type sponsoring      | {{ $data->type }}                                 |
+| Bedrag               | € {{ number_format($data->amount, 2, ',', ',') }} |
+| Frequentie           | {{ $data->frequency }}                            |
+| Voornaam             | {{ $data->firstname }}                            | 
+| Tussenvoegsel(s)     | {{ $data->infix }}                                | 
+| Achternaam           | {{ $data->surname }}                              |
+| E-mailadres          | {{ $data->email }}                                |
+| Telefoonnnummer      | {{ $data->phone }}                                |
+| Naam rekeninghouder  | {{ $data->accountHolder }}                        |
+| IBAN                 | {{ $data->iban }}                                 |
+| Nieuwsbrief?         | {{ $data->newsletter ? 'Ja' : 'Nee' }}            |
+| Privacyverklaring?   | {{ $data->privacy ? 'Ja' : 'Nee' }}               |
+</x-mail::table>
+
+<br /><br />
+Een vriendelijke groet,<br>
+Het team van {{ config('app.name') }}
+</x-mail::message>
