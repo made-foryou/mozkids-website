c41e7762d374a771fbb830748f6a100a8934bd9a
Menno Tempelaar <menno@made-foryou.nl>
2025-06-25 18:59:57 +0200

    :lipstick: Fix styling for three column strips.

--- resources/views/components/columns/image.blade.php
+++ resources/views/components/columns/image.blade.php
@@ -1,8 +1,9 @@
 @props([
     'image' => '',
     'alt' => '',
+    'columns' => 1,
 ])
 
-<figure class="w-full h-full">
-    <img src="{{ $image }}" alt="{{ $alt ?? '' }}" class="object-cover object-center rounded-lg w-full h-full" />
+<figure class="w-full @if ($columns === 3) h-auto @else h-full @endif">
+    <img src="{{ $image }}" alt="{{ $alt ?? '' }}" class="object-cover object-center rounded-lg @if ($columns === 3) w-full h-[250px] @else w-full h-full @endif" />
 </figure>
