1428142219a447667834931aa171e61e4fa21e45
Menno Tempelaar <menno@made-foryou.nl>
2025-11-29 17:50:46 +0100

    :art: Add styling updates, new route, and code adjustments for improved functionality

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+use Made\Cms\Analytics\Commands\ClearVisitLogsCommand;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
