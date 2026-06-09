a16c4f731a10f6d1da26d4804f6381c0db34f53a
Menno Tempelaar <menno@made-foryou.nl>
2026-06-09 13:57:04 +0200

    :lipstick: Upgrade text strip.

--- resources/views/strips/text.blade.php
+++ resources/views/strips/text.blade.php
@@ -1,12 +1,30 @@
-<section
-    class="relative
-           w-full
-           py-8
-           lg:py-12"
->
-    <x-container class="max-w-5xl prose">
-        <h1>{{ $title }}</h1>
+<section class="text-strip relative
+                w-full
+                pt-3 pb-8
+                lg:pt-4 lg:pb-12">
 
-        {!! str($content)->sanitizeHtml() !!}
+    <x-container class="max-w-4xl">
+
+        @if (!empty($title))
+            <h1 class="text-strip__title
+                       block mb-6
+                       text-3xl lg:text-4xl
+                       text-secondary-900 font-semibold
+                       tracking-[-0.018em] leading-[1.15]
+                       text-balance"
+                data-reveal="fade-up"
+                style="--reveal-delay: 0ms">
+                {{ $title }}
+            </h1>
+        @endif
+
+        <div class="prose max-w-none text-strip__prose
+                    text-secondary-900/82
+                    [&>p:first-child]:mt-0"
+             data-reveal="fade-up"
+             style="--reveal-delay: {{ !empty($title) ? 120 : 0 }}ms">
+            {!! str($content)->sanitizeHtml() !!}
+        </div>
+
     </x-container>
 </section>
